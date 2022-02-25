eb229c854328e4df998c92cfa90179be4660c70d
situ2001 <yongcong2001@outlook.com>
2022-02-25 17:27:46 +0800

    chapter7: interpose

---
+++ chapter7/interpose/mymalloc.c
@@ -0,0 +1,89 @@
+#ifdef COMPILETIME
+#include <stdio.h>
+#include <malloc.h>
+
+void *mymalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    printf("malloc(%d)=%p\n", (int)size, ptr);
+    return ptr;
+}
+
+void myfree(void *ptr)
+{
+    free(ptr);
+    printf("free(%p)\n", ptr);
+}
+
+#endif
+
+#ifdef LINKTIME
+#include <stdio.h>
+
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+
+void *__wrap_malloc(size_t size)
+{
+    void *ptr = __real_malloc(size);
+    printf("malloc(%d)=%p\n", (int)size, ptr);
+    return ptr;
+}
+
+void __wrap_free(void *ptr)
+{
+    __real_free(ptr);
+    printf("free(%p)\n", ptr);
+}
+#endif
+
+// #define RUNTIME
+
+#ifdef RUNTIME
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+void *malloc(size_t size)
+{
+    void *(*mallocp)(size_t size);
+    char *error;
+
+    mallocp = dlsym(RTLD_NEXT, "malloc");
+    if ((error = dlerror()) != NULL)
+    {
+        fputs(error, stderr);
+        exit(1);
+    }
+    char *ptr = mallocp(size);
+    static char malloc_in_progess = 0;
+    if (!malloc_in_progess)
+    {
+        malloc_in_progess = 1;
+        printf("malloc(%d)=%p\n", (int)size, ptr);
+        malloc_in_progess = 0;
+    }
+    return ptr;
+}
+
+void free(void *ptr)
+{
+    void (*freep)(void *ptr);
+    char *error;
+
+    if (ptr == NULL)
+    {
+        return;
+    }
+
+    freep = dlsym(RTLD_NEXT, "free");
+    if ((error = dlerror()) != NULL)
+    {
+        fputs(error, stderr);
+        exit(1);
+    }
+    freep(ptr);
+    printf("free(%p)\n", ptr);
+}
+#endif
